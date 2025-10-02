982b5189b7d21e34a5a9205c8910225a36920113
idolienc <gwardgaming@gmail.com>
2025-10-02 10:01:14 +0100

    Added the ability to generate unsorted lists of 10000 elements and then time the sorts and compare them together

--- algorithms.py
+++ algorithms.py
@@ -1,3 +1,5 @@
+import time, random
+
 def bubble_sort(mylist: list) -> list:
     for i in range(len(mylist)-1):
         for x in range(len(mylist)-(1+i)):
@@ -11,9 +13,33 @@ def insertion_sort(mylist: list) -> list:
         insert_into_sorted(sortedlist, mylist[i])
     return sortedlist
 
-def insert_into_sorted(the_list, element):
-    for i in range(len(the_list)):
-        if element < the_list[i]:
-            the_list.insert(i, element)
+def insert_into_sorted(mylist: list, element: int):
+    for i in range(len(mylist)):
+        if element < mylist[i]:
+            mylist.insert(i, element)
             return
-    the_list.append(element)
+    mylist.append(element)
+
+def generate_unsorted_list():
+    unsorted_list = []
+    for i in range(10000):
+        unsorted_list.append(random.randint(1, 10000))
+    return unsorted_list
+
+def time_sort(sorting_method, mylist):
+    start = time.time()
+    sorted_list = sorting_method(mylist.copy()) 
+    end = time.time()
+    elapsed_time = end - start
+    return sorted_list, elapsed_time
+
+def main():
+    sort_list = generate_unsorted_list()
+    bubble_sort_result, bubble_elapsed = time_sort(bubble_sort, sort_list)
+
+    insertion_sort_result, insertion_elapsed = time_sort(insertion_sort, sort_list)
+
+    print(f"Insertion: Sorted list in {bubble_elapsed:.4f} seconds")
+    print(f"Bubble: Sorted list in {insertion_elapsed:.4f} seconds")
+
+main()
