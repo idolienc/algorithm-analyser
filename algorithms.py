3dbdf90d423eeb42a44a95e116d2e1ac9ffdb78f
idolienc <gwardgaming@gmail.com>
2025-10-08 20:38:28 +0100

    Added merge sort, reworked how sorting algorithms recieve unsorted lists and made the graph look better using matplotlib cyberpunk style.

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,6 @@
 import time, random
 import matplotlib.pyplot as plt
+import mplcyberpunk
 from collections.abc import Callable
 from typing import TypeVar
 
@@ -32,6 +33,39 @@ def insertion_sort(lst: list[T]) -> None:
             j -= 1
         i += 1
 
+def merge_sort(lst: list[T]) -> list[T]:
+    """Sorts a list by recursively splitting the input 
+    list down into single elements, merging two
+    sorted sublists until a fully sorted list is produced.
+
+    Time complexity is O(n logn)
+    """
+    if len(lst) <= 1:
+        return lst.copy()
+    left_lst = merge_sort(lst[:len(lst)//2])  
+    right_lst = merge_sort(lst[len(lst)//2:])  
+    return merge(left_lst, right_lst)
+
+def merge(left: list[T], right: list[T]) -> list[T]:
+    """Merges two sorted lists into a single sorted list.
+    Compares the smallest elements and appends the smaller one 
+    to a new list. Continues until all elements are ordered. 
+    Assumes both lists are already ordered.
+    """
+    result=[]
+    l_index = 0
+    r_index = 0
+    while l_index < len(left) and r_index < len(right):
+        if left[l_index] <= right[r_index]:
+            result.append(left[l_index])
+            l_index += 1
+        else:
+            result.append(right[r_index])
+            r_index += 1
+    result.extend(left[l_index:])
+    result.extend(right[r_index:])
+    return result
+
 def generate_unsorted_list(size: int) -> list[int]:
     """Generates an unsorted list containing 
     integers ranging from 1 to 1,000,000
@@ -56,6 +90,7 @@ def graph_efficiency(element_count: list, **kwargs):
     """Plots a log-scaled comparison of execution times for two sorting algorithms 
     over varying input sizes.
     """
+    plt.style.use('cyberpunk')
     plt.title('Sort Elapsed Time Comparison')
     plt.xlabel('Number of Elements')
     plt.ylabel('Time Taken (seconds)')
@@ -67,6 +102,7 @@ def graph_efficiency(element_count: list, **kwargs):
     for arg in kwargs:
         plt.plot(element_count, kwargs[arg], label=(arg.replace("_", " ")).title(), marker='o')
     plt.legend(loc="upper left")
+    mplcyberpunk.add_glow_effects()
     plt.show()
 
 def main():
@@ -74,18 +110,23 @@ def main():
     unsorted_lists = []
 
     insertion_times = []
+    bubble_times = []
+    merge_times = []
     for i in range(len(element_count)):
         unsorted_lists.append(generate_unsorted_list(element_count[i]))
-        insertion_sort_result, insertion_elapsed = timer(insertion_sort, unsorted_lists[i])
+        unsorted_insertion = unsorted_lists.copy()
+        unsorted_bubble = unsorted_lists.copy()
+        unsorted_merge = unsorted_lists.copy()
+        insertion_sort_result, insertion_elapsed = timer(insertion_sort, unsorted_insertion[i])
         insertion_times.append(insertion_elapsed)
 
-    bubble_times = []
-    for i in range(len(element_count)):
-        unsorted_lists.append(generate_unsorted_list(element_count[i]))
-        bubble_sort_result, bubble_elapsed = timer(bubble_sort, unsorted_lists[i])
+        bubble_sort_result, bubble_elapsed = timer(bubble_sort, unsorted_bubble[i])
         bubble_times.append(bubble_elapsed)
 
-    graph_efficiency(element_count, insertion_times=insertion_times, bubble_times=bubble_times)
+        merge_sort_result, merge_elapsed = timer(merge_sort, unsorted_merge[i])
+        merge_times.append(merge_elapsed)
+
+    graph_efficiency(element_count, insertion_times=insertion_times, bubble_times=bubble_times, merge_times=merge_times)
 
 if __name__ == "__main__":
     main()
